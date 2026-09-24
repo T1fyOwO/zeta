ecd4baf17ead73017df2cf694a816c4451c2f907
T1fyOwO <t1fyowo@gmail.com>
2026-09-24 17:15:22 -0500

    Update Zeta_Team_lib_zeta.py

--- lib_files/zeta_team/Zeta_Team_lib_zeta.py
+++ lib_files/zeta_team/Zeta_Team_lib_zeta.py
@@ -1,5 +1,16 @@
 # Zeta_Team_lib_zeta.py
+# Library name: lib_zeta-team_zeta
+
+LIB_NAME = "lib_zeta-team_zeta"
+FORCED = True
 
 LIBRARIES = {
     "lib_zeta_variables": "lib_files/zeta_team/Zeta-Team_variables.py"
 }
+
+
+def load():
+    if not FORCED:
+        raise RuntimeError("lib_zeta-team_zeta must be forced to load.")
+
+    return True
