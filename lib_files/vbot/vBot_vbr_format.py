af6a74e4b4c8a06bebd2801cbe998dcd75bdab81
T1fyOwO <t1fyowo@gmail.com>
2026-09-24 17:58:18 -0500

    Create vBot_vbr_format.py

---
+++ lib_files/vbot/vBot_vbr_format.py
@@ -0,0 +1,16 @@
+# vBot_vbr_format.py
+
+FORMAT_NAME = "vbr"
+FILE_EXTENSION = ".vbr"
+
+SUPPORTED_COMMANDS = {
+    "i",
+    "r",
+}
+
+COMMANDS = {
+    "i": "input",
+    "r": "release",
+}
+
+FRAME_SYNTAX = "frame:{value}"
